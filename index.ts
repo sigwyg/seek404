e7861fe2ee9d7c4fc17419f7df2e52ee87b19bb2
sigwyg <sigwyg@users.noreply.github.com>
2022-07-06 19:41:47 +0900

    サイト内リンクを抽出

--- index.ts
+++ index.ts
@@ -12,26 +12,49 @@ async function getHtmlDom(url) {
     return dom
 }
 
-function getLinksText(dom, origin_url) {
+function getLinkAll(dom, origin_url) {
     const urls = []
     dom.querySelectorAll("a").forEach((link) => {
         urls.push(link.getAttribute("href"))
     });
-    const filtered_urls = urls.filter(url => {
-        const regexp = new RegExp(origin_url)
-        if(regexp.test(url)) { return true }
+    return urls
+}
 
-        if(url.match(/^\.?\//)) { // 相対リンクか絶対リンク
-            return true
+/**
+ * filtererdLinkAll()
+ * リンクリストから、サイト内リンクを抜き出す
+ * 
+ * @param {string[]} urls a[href]の中身
+ * @param {string} origin 対象サイトのorigin
+ * @return {string[]} サイト内リンク
+ *
+ */
+function filteredLinkAll(urls, origin) {
+    const filtered_urls = urls.map(url => {
+        // 相対/絶対リンクならoriginを付加する
+        // @example "./about" -> "http://google.com/about"
+        const regexp_inside = new RegExp(/^\.?\//)
+        if(regexp_inside.test(url)) {
+            url = url.replace(regexp_inside, "")
+            origin = origin.replace(/[^/]$/, "/")
+            url = origin + url
         }
 
-        return false
+        // http...でoriginと一致するなら返却
+        const regexp = new RegExp(origin)
+        if(regexp.test(url)) { return url }
+
+        return ""
     })
-    return filtered_urls
+    return filtered_urls.filter(Boolean)
 }
 
 const url = "https://gaiheki-madoguchi.com/"
-const html = await getHtmlDom(url)
 const origin_url = url.match(/https?:\/\/[^/]+\//)[0];
-const site_links = getLinksText(html, origin_url)
-console.log(site_links)
+
+const html = await getHtmlDom(url)
+const links = getLinkAll(html)
+
+let links_inside = new Map()
+links_inside = filteredLinkAll(links, origin_url)
+console.log(links_inside)
