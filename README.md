f571bd4add40a6f1de8b4cf418d51073baacf237
sigwyg <sigwyg@users.noreply.github.com>
2022-07-06 12:45:41 +0900

    denoでリンク一覧を出す

---
+++ README.md
@@ -0,0 +1,5 @@
+
+## Getting Started
+
+1. Install [Deno](https://deno.land/manual/getting_started/installation)
+2. `deno run -A index.ts`
--- index.ts
+++ index.ts
@@ -1,19 +1,26 @@
-const puppeteer = require('puppeteer');
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 
-const vgmUrl = 'https://www.vgmusic.com/music/console/nintendo/nes';
+async function getHtmlDom(url) {
+    const response = await fetch(url).catch((err) => {
+        console.error("URL parse failed!")
+    })
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+    const html_text = await response.text()
+    const dom = new DOMParser().parseFromString(html_text, "text/html");
+    if (!dom) { throw new Error("DOM parse failed"); }
 
-  await page.goto(vgmUrl);
+    return dom
+}
 
-  const links = await page.$$eval('a', elements => elements.filter(element => {
-    const parensRegex = /^((?!\().)*$/;
-    return element.href.includes('.mid') && parensRegex.test(element.textContent);
-  }).map(element => element.href));
+function getLinksText(dom, origin_url) {
+    const urls = []
+    dom.querySelectorAll("a").forEach((link) => {
+        urls.push(link.getAttribute("href"))
+    });
+    return urls
+}
 
-  links.forEach(link => console.log(link));
-
-  await browser.close();
-})();
+const url = "https://gaiheki-madoguchi.com/"
+const html = await getHtmlDom(url)
+const site_links = getLinksText(html, origin_url)
+console.log(site_links)
