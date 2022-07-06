0a29fd9842d1f3735fc3b857880e8bcb21a53232
sigwyg <sigwyg@users.noreply.github.com>
2022-07-06 21:09:19 +0900

    ファイル分割

---
+++ util.ts
@@ -0,0 +1,88 @@
+/**
+ * getHtmlDom()
+ * urlを受け取って、DOMを返す
+ * 
+ * @param {string} url対象サイトのURL
+ * @return {dom} 対象サイトの、DOM解析済みのオブジェクト
+ *
+ */
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+export async function getHtmlDom(url) {
+    const response = await fetch(url).catch((err) => {
+        console.error("URL parse failed!")
+    })
+
+    const html_text = await response.text()
+    const dom = new DOMParser().parseFromString(html_text, "text/html");
+    if (!dom) { throw new Error("DOM parse failed"); }
+
+    return dom
+}
+
+
+/**
+ * getLinkAll()
+ * WebページのDOMから、a[href]を抜き出して返却
+ * 
+ * @param {DOM} dom 対象サイトの、DOM解析済みのオブジェクト
+ * @return {string[]} サイト内リンクの一覧
+ *
+ */
+export function getLinkAll(dom) {
+    const urls = []
+    dom.querySelectorAll("a").forEach((link) => {
+        urls.push(link.getAttribute("href"))
+    });
+    return urls
+}
+
+/**
+ * filtererdLinkAll()
+ * リンクリストから、サイト内リンクを抜き出す
+ * 
+ * @param {string[]} urls a[href]の中身
+ * @param {string} origin 対象サイトのorigin
+ * @return {string[]} サイト内リンク
+ *
+ */
+export function filteredLinkAll(urls, origin) {
+    const filtered_urls = urls.map(url => {
+        // 相対/絶対リンクならoriginを付加する
+        // @example "./about" -> "http://google.com/about"
+        const regexp_inside = new RegExp(/^\.?\//)
+        if(regexp_inside.test(url)) {
+            url = url.replace(regexp_inside, "")
+            origin = origin.replace(/[^/]$/, "/")
+            url = origin + url
+        }
+
+        // http...でoriginと一致するなら返却
+        const regexp = new RegExp(origin)
+        if(regexp.test(url)) { return url }
+
+        return ""
+    })
+    return filtered_urls.filter(Boolean)
+}
+
+/**
+ * check404()
+ * リンクリストをfetchして、statusをチェックする
+ * 
+ * @param {string[]} urls リンクリスト
+ * @return {Map} リンクリストをkey、status codeをvalueとしてもの
+ *
+ */
+export async function check404(urls) {
+    const results = await Promise.all(
+        urls.map(async (url) => {
+            const result = await fetch(url)
+            return [ url, result.status ]
+        })
+    )
+    let checked = new Map()
+    results.forEach(result => {
+        checked.set(result[0], result[1])
+    })
+    return checked
+}
